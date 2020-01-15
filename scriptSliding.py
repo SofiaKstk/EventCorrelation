0aa4ce8b69af5b101230c91c9c71fbfbd6409b22
Thomais Vasilopoulou <thomaisv97@gmail.com>
2020-01-15 19:49:40 +0200

    full script to run sliding window algorithm

--- scriptSliding.py
+++ scriptSliding.py
@@ -1,13 +1,19 @@
 import os
 import numpy as np
+import os.path
+from os import path
 
 
-for k in range(1, 8):
-	for w in range(3, 8):
-		for p in np.arange(0.4, 1.0, 0.1):
-			for step in [1, 2, 3]:
-				file = "results/sliding/step" + str(k) + str(w) + str(step) + ".txt"
-				print("slidingWindow.py " + str(k) + " " + str(w) + " " + str(step) + " " + file)
-				os.system("slidingWindow.py " + str(k) + " " + str(w) + " " + str(p) + " " + str(step) + " " + file)
+# for k in range(4, 8):
+k=4
+for w in range(6, 8):
+	for p in np.arange(0.4, 1.0, 0.1):
+		for step in [1, 2, 3]:
+			for algorithm in ["cusum", "shewhart"]:
+				file = "results/sliding/" + algorithm + "/step" + str(k) + str(w) + str(step) + "p" + str(p) + ".txt"
+				print("slidingWindow.py " + str(k) + " " + str(w) + " " + str(step) + " " +algorithm + " " + file)
+				if str(path.exists(file)) == "True":
+					continue
+				os.system("slidingWindow.py " + str(k) + " " + str(w) + " " + str(p) + " " + str(step) + " " + file + " " + algorithm)
 
 	# k,w,p,steps,file,algorithm
